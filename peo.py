46d365719e6bcc769a326e4c199c38c3f0c5ee32
James Freeman <sysadmin@hkskies.com>
2024-02-21 16:36:14 +0000

    Added ability to encrypt and decrypt using a password as well as a key

--- peo.py
+++ peo.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python3
 import argparse
 from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import os
 import base64
 from itertools import cycle
+import getpass
+import binascii
 
 def generate_key(key_file):
     """Generates a key and saves it into a file"""
     key = Fernet.generate_key()
     with open(key_file, "wb") as key_file:
         key_file.write(key)
+
+def generate_key_from_password(password_provided, salt):
+    """Generates a Fernet key from a provided password."""
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,  # Fernet keys are 32 bytes long
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password_provided.encode()))  # Password must be bytes
+    return key
 
 def load_key(key_file):
     """Loads the previously generated key"""
@@ -58,24 +75,33 @@ def deobfuscate_data(data, key, insert_dummy_every=10):
     
     return bytes(original_data)
 
-def encrypt_file(input_file_name, output_file_name, key):
+def encrypt_file(input_file_name, output_file_name, key, salt=None):
     """Encrypts and obfuscates a file"""
     fernet = Fernet(key)
     
     with open(input_file_name, "rb") as file:
         file_data = file.read()
         encrypted_data = fernet.encrypt(file_data)
-        obfuscated_data = obfuscate_data(encrypted_data, key)
+        if salt:
+            obfuscated_data = salt + obfuscate_data(encrypted_data, key)
+        else:
+            obfuscated_data = obfuscate_data(encrypted_data, key)
 
     with open(output_file_name, "wb") as file:
         file.write(obfuscated_data)
 
-def decrypt_file(input_file_name, output_file_name, key):
+def decrypt_file(input_file_name, output_file_name, key=None, password=None):
     """Deobfuscates and decrypts a file"""
-    fernet = Fernet(key)
     
     with open(input_file_name, "rb") as file:
-        obfuscated_data = file.read()
+        if password:
+            salt = file.read(16)
+            key = generate_key_from_password(password, salt)
+            fernet = Fernet(key)
+            obfuscated_data = file.read()
+        else:
+            fernet = Fernet(key)
+            obfuscated_data = file.read()
         deobfuscated_data = deobfuscate_data(obfuscated_data, key)
         decrypted_data = fernet.decrypt(deobfuscated_data)
 
@@ -83,14 +109,24 @@ def decrypt_file(input_file_name, output_file_name, key):
         file.write(decrypted_data)
 
 def main():
+    # Initialize variables
+    key = None
+    password = None
+    salt = None
+
     parser = argparse.ArgumentParser(description="Encrypt or Decrypt a file using Fernet encryption with advanced obfuscation.")
-    parser.add_argument("-e", "--encrypt-file", type=str, help="The name of the file to perform the operation on.")
+    opgroup = parser.add_mutually_exclusive_group(required=True)
+    opgroup.add_argument("-e", "--encrypt-file", type=str, help="The name of the file to perform the operation on.")
+    opgroup.add_argument("-d", "--decrypt-file", type=str, help="Decrypt the input file. If not specified, the default action is to encrypt.")
+
     parser.add_argument("-o", "--output-file", type=str, help="The name of the output file. Defaults to 'output.enc' for encryption or 'output.dec' for decryption if not provided.")
-    parser.add_argument("-d", "--decrypt-file", type=str, help="Decrypt the input file. If not specified, the default action is to encrypt.")
+    
+    # Add a parser argument to tell the program to generate a new key
+    opgroup.add_argument("-g", "--generate-key", action="store_true", help="Generate a new secret key and save it to a file")
+    # Add a parser argument to retrieve a password from the user
+    parser.add_argument("-p", "--password", nargs='?', const="", help="The password to use for key generation")
     # Add a parser argument to accept a filename for the key file
     parser.add_argument("-k", "--key-file", type=str, default="secret.key", help="The name of the file containing the secret key")
-    # Add a parser argument to tell the program to generate a new key
-    parser.add_argument("-g", "--generate-key", action="store_true", help="Generate a new secret key and save it to a file")
 
     args = parser.parse_args()
 
@@ -102,16 +138,33 @@ def main():
         print(f"Generated a new key and saved it to '{args.key_file}'.")
         # Exit the function and finish processing
         return
-
-    key = load_key(args.key_file)  # Ensure a key is already generated
-
+   
+    # If the user specifies the password argument with no value, prompt the user for a password
+    if args.password == "":
+        password = getpass.getpass("Enter a password: ").replace("\n", "").replace("\r", "")
+    # If the user specifies the password argument with a value, use that value
+    elif args.password:
+        password = args.password
+    
+    if password and args.encrypt_file:
+        salt = os.urandom(16)
+        key = generate_key_from_password(password, salt)
+    elif password and args.decrypt_file:
+        pass
+    elif not args.password and not password and (args.encrypt_file or args.decrypt_file):
+        key = load_key(args.key_file)  # Ensure a key is already generated
+    else:
+        print("No key or password provided.")
+        return
+    
+    print(binascii.hexlify(password.encode()))
     if args.decrypt_file:
         output_file = args.output_file if args.output_file else "output.dec"
-        decrypt_file(args.decrypt_file, output_file, key)
+        decrypt_file(args.decrypt_file, output_file, key, password)
         print(f"Decrypted '{args.decrypt_file}' to '{output_file}'.")
     else:
         output_file = args.output_file if args.output_file else "output.enc"
-        encrypt_file(args.encrypt_file, output_file, key)
+        encrypt_file(args.encrypt_file, output_file, key, salt)
         print(f"Encrypted '{args.encrypt_file}' to '{output_file}'.")
 
     
